88e588f6ce42a80da7d1a54704214ea6aad8313b
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-12-07 11:35:39 -0600

    Agregando paquete de login y registro

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegistroController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Investigador;
-use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +17,16 @@ use Illuminate\Support\Facades\DB;
 
 Route::view('/', 'bienvenida')->name('bienvenida');
 Route::get('/registro', [RegistroController::class, 'inicio'])->name('registro.inicio');
-Route::get('/municipios/{idDepartamento}', [RegistroController::class, 'listarMunicipios'])->name('municipios.listar');
-Route::post('/investigadores/registro', [RegistroController::class, 'guardarInvestigador'])->name('registro.guardar');
-Route::get('/prueba', function() {
-    $nTramite = DB::select("SELECT `AUTO_INCREMENT` as tramite FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_SCHEMA = 'investigadores_cyt'
-    AND TABLE_NAME = 'investigadores';");
-    return $nTramite[0]->tramite;
+Route::post('/registro', [RegistroController::class, 'guardarInvestigador'])->name('registro.guardar');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
